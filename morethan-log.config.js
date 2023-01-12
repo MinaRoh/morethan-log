4790fd9c80db98c6ac96d80ccf9137e4f48a2b5e
MinaRoh <00minha@gmail.com>
2023-01-12 21:25:52 +0900

    update config.js utterance repo

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: 'morethanmin/morethan-log',
+      repo: 'MinaRoh/notion-utterances',
       'issue-term': 'og:title',
       label: '💬 Utterances',
     }
